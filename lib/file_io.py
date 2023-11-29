966c62d27c098c5e71f56cab589e8257471f2ddd
mjainamshah <jainam.shah@student.moringaschool.com>
2023-11-29 13:40:04 +0300

    COMPLETED

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
+    with open(f'{file_name}.txt', 'w', encoding='utf-8') as f:
+        f.write(file_content)
     pass
 
 def append_file(file_name, append_content):
+    with open(f'{file_name}.txt', 'a', encoding='utf-8') as f:
+        f.write(append_content)
     pass
 
 def read_file(file_name):
+    with open(f'{file_name}.txt', encoding='utf-8') as f:
+        return f.read()
     pass
